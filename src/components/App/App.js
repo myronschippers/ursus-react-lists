cb990efc63a08a95050a97be1a3c5bdb20ee941c
Myron <dubtwee@gmail.com>
2020-04-09 08:11:46 -0500

    reverting codebase for re-recording

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,84 +2,12 @@ import React, { Component } from 'react';
 import Header from '../Header/Header';
 import './App.css';
 
-const list = [1,2,3,4];
-
-const newlist = [...list, 6];
-// [1,2,3,4,6]
-
-const obj = {
-  name: 'Myron',
-}
-
-const newObj = {
-  ...obj,
-  age: 37
-}
-// {
-//   name: 'Myron',
-//   age: 37
-// }
-
 class App extends Component {
-  state ={
-    creatures: [
-      {
-        name: 'Unicorn',
-        origin: 'Kansas City',
-      },
-      {
-        name: 'Sphinx',
-        origin: 'Egypt',
-      },
-      {
-        name: 'Kappa',
-        origin: 'Japan',
-      },
-      {
-        name: 'Dryad',
-        origin: 'Isles',
-      }
-    ],
-  }
 
   render() {
-    // const creatureElements = [];
-
-    // for (let i = 0; i < this.state.creatures.length; i++) {
-    //   creatureElements.push(
-    //     <li>{this.state.creatures[i]}</li>
-    //   );
-    // }
-
-    // forEach LOOP
-    // this.state.creatures.forEach((item, index) => {
-    //   creatureElements.push(<li>{item}</li>);
-    // });
-
-    // map LOOP
-    const creatureElements = this.state.creatures.map((item, index) => {
-      return <li key={index}>{item.name} is from {item.origin}</li>;
-    });
-    // const creatureElements = this.state.creatures.map((item) => <li>{item}</li>);
-
     return (
       <div>
         <Header />
-
-        <div className="container">
-          {/* this.state.creatures */}
-
-          <div>
-            <h2>Creature Elements:</h2>
-            <ul>
-              {creatureElements}
-            </ul>
-          </div>
-
-          <pre>
-            {JSON.stringify(this.state.creatures)}
-          </pre>
-        </div>
       </div>
     );
   }
