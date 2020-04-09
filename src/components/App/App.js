11f1c14304f403c5cbb286b37a7dcb8c7670d434
Myron <dubtwee@gmail.com>
2020-04-09 08:34:56 -0500

    using lists in react

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,11 +3,46 @@ import Header from '../Header/Header';
 import './App.css';
 
 class App extends Component {
+  state = {
+    creaturesList: ['Unicorn', 'Sphinx', 'Kappa', 'Dryad']
+  }
 
   render() {
+    // restructure array to use elements for visual presentation
+    // const creatureElements = [];
+
+    // using basic for loop
+    // for (let i = 0; i < this.state.creaturesList.length; i++) {
+    //   creatureElements.push(
+    //     <li>{this.state.creaturesList[i]}</li>
+    //   );
+    // }
+
+    // using forEach loop
+    // this.state.creaturesList.forEach((item, index) => {
+    //   creatureElements.push(
+    //     <li key={index}>{item}</li>
+    //   );
+    // });
+
+    // using map loop
+    const creatureElements = this.state.creaturesList.map((item, index) => {
+      return <li key={index}>{item}</li>;
+    });
+    // const creatureElements = this.state.creaturesList.map((item, index) => <li key={index}>{item}</li>);
+
     return (
       <div>
         <Header />
+
+        <div className="container">
+          {this.state.creaturesList}
+
+          <h2>Creature Elements</h2>
+          <ul>
+            {creatureElements}
+          </ul>
+        </div>
       </div>
     );
   }
